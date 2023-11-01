81b7606a561d053d75b881e9e8923c61744e7543
Iago Silva <iagostv1@gmail.com>
2023-10-31 23:46:13 -0300

    Pensar e responder rotas angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RootModuleModule } from './root-module/root-module.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'root', loadChildren: () => RootModuleModule }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
